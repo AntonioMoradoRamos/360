40477eefb7318159d43790c2e40818565db06577
aramos <Antonio.Ramos@i2s.pt>
2019-02-07 12:13:32 +0000

    estilos e fluxo

--- js/laco.js
+++ js/laco.js
@@ -3,39 +3,39 @@ function addOnInputEventOnInput() {
     // Submeter o formulário
     jQuery("#pesqGatinhos").on('input', function (event) {
 
-        jQuery(".images-container").html(''); 
-        jQuery(".images-container").append('<p><img class="imgDefault" src=default.gif></p>'); 
-        clearTimeout(myTimeOut); 
+        jQuery(".images-container").html('');
+        jQuery(".images-container").append('<p><img class="imgDefault" src=default.gif></p>');
+        clearTimeout(myTimeOut);
         myTimeOut = setTimeout(function () {
 
 
 
-            let pesquisar = jQuery("#pesqGatinhos").val(); 
+            let pesquisar = jQuery("#pesqGatinhos").val();
             jQuery.ajax(
                 "http://api.giphy.com/v1/gifs/search?q=+" + pesquisar + "+&limit=10&api_key=dc6zaTOxFJmzC").done(function (result) {
-                console.log(result); 
-                jQuery(".images-container").html(''); 
-                result.data.forEach(element =>  {
-                    const url2show = element.images.original.url; 
-                    jQuery(".images-container").append('<p><img class="main img" src=' + url2show + '></p>'); 
-                }); 
+                console.log(result);
+                jQuery(".images-container").html('');
+                result.data.forEach(element => {
+                    const url2show = element.images.original.url;
+                    jQuery(".images-container").append('<p><img class="main img" src=' + url2show + '></p>');
+                });
 
-            }); 
-        }, 3500); 
+            });
+        }, 3500);
 
-    }); 
+    });
 
 }
 
-const ID_INPUT_VALUE = '#id-form-laco-submit'; 
+const ID_INPUT_VALUE = '#id-form-laco-submit';
 
 function getElementId(idElement) {
-    return document.querySelector(idElement); 
+    return document.querySelector(idElement);
 }
 
 function addEventListener2ButtonLaco() {
-    let lstBotao = getElementId(ID_INPUT_VALUE); 
-    lstBotao.addEventListener("click", addOnInputEventOnInput); 
+    let lstBotao = getElementId(ID_INPUT_VALUE);
+    lstBotao.addEventListener("click", addOnInputEventOnInput);
 
 
 }
@@ -44,36 +44,43 @@ function addAction2Form(idForm, theAction) {
 
     //alert(idForm); 
     //alert(theAction); 
-    jQuery(idForm).attr("action", theAction); 
-    
+    jQuery(idForm).attr("action", theAction);
+
 }
 
 function addEventOnSubmit2Button(idButton) {
-    
+
     // Alterar o conteúdo em função do click
     jQuery(idButton).on('click', function (event) {
-        event.preventDefault(); 
-        
+        event.preventDefault();
+
         //alert('submit'); 
-        
+
         /*jQuery('#form-escolha-1').submit(function () {
             alert('submit'); 
-        });*/ 
-           
-        jQuery('#form-escolha-1').submit(); 
+        });*/
+
+        jQuery('#form-escolha-1').submit();
         //this.submit(); 
-        
-    }); 
+
+    });
 }
 
 // Página escolha-1
-addAction2Form('#form-escolha-1', '/projecto.360.io/escolha-2.html'); 
-addEventOnSubmit2Button('#id-btn-to-escolha-2'); 
+addAction2Form('#form-escolha-1', '/projecto.360.io/escolha-2.html');
+addEventOnSubmit2Button('#id-btn-to-escolha-2');
 
 // Página escolha-2
-addAction2Form('#form-escolha-2', '/projecto.360.io/form-1.html'); 
-addEventOnSubmit2Button('#id-btn-to-form-1'); 
- 
+addAction2Form('#form-escolha-2', '/projecto.360.io/form-1.html');
+addEventOnSubmit2Button('#id-btn-to-form-1');
+
+// Página form-1
+addAction2Form('#form-1-laco', '/projecto.360.io/form-2.html');
+addEventOnSubmit2Button('#id-btn-to-form-2');
+
+// Página form-2
+addAction2Form('#form-2-laco', '/projecto.360.io/parabens.html');
+addEventOnSubmit2Button('#id-btn-to-parabens');
 
 //addEventListener2ButtonVoltar(); 
 
